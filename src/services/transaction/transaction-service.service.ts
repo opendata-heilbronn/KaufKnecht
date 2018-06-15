b916c3f65002b86ac5d8735d255bacc258d0e41f
Niklas Portmann <niklas.portmann@kaufland.com>
2018-06-16 01:10:44 +0200

    ehh services zzzz

--- src/services/transaction/transaction-service.service.ts
+++ src/services/transaction/transaction-service.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { TransactionModel } from '../../app/models/transaction.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionServiceService {
 
-  constructor(db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase) {
+  }
+
+  // Promise<<TransactionModel>[]>
+  getTransactions() {
+    let transactions: TransactionModel[] = [];
+    let observable = Observable.create(observer => {
+      this.db.database.ref().child('data').on('child_added', (snapshot) => {
+
+      });
+    });
   }
 }
