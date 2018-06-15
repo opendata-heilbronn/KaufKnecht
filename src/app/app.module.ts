4ae436fe25e418700a02596d235d2fbd0a660060
Niklas Portmann <niklas.portmann@kaufland.com>
2018-06-15 21:24:19 +0200

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDV_bWdq0FrkczVAMtVKRFGeANqVmktXPI",
+    authDomain: "kaufkraft-b6de2.firebaseapp.com",
+    databaseURL: "https://kaufkraft-b6de2.firebaseio.com",
+    projectId: "kaufkraft-b6de2",
+    storageBucket: "kaufkraft-b6de2.appspot.com",
+    messagingSenderId: "363811015684"
+  }
 };
