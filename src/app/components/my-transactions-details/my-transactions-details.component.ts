2918e621b0813f6f29566195152ac6d626121963
LeoDJ <leandro.spaeth@gmail.com>
2018-06-16 01:23:59 +0200

    implement transaction detail view with mock data

--- src/app/components/my-transactions-details/my-transactions-details.component.ts
+++ src/app/components/my-transactions-details/my-transactions-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductModel } from '../../models/product.model';
+import { ListEntryModel } from '../../models/list-entry.model';
 
 @Component({
   selector: 'app-my-transactions-details',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyTransactionsDetailsComponent implements OnInit {
 
-  constructor() { }
+  private mockItems: ListEntryModel[] = [];
+  private mockItem: ProductModel = new ProductModel();
+  itemList: any[] = [];
+
+
+  getProductByID(id: string): ProductModel {
+    if (id == "1") {
+      return this.mockItem;
+    }
+    else return null;
+  }
+
+  constructor(private route: ActivatedRoute) {
+
+    //mock data
+    this.mockItem.id = "1";
+    this.mockItem.description = "z.B. Rittersporn oder Löwenmäulchen, Topf: 13 cm ø";
+    this.mockItem.title = "VitaSafe Melonen-Mix";
+    this.mockItem.images = ["https://media.kaufland.com/images/PPIM/Lago/c16221167_6.jpg"];
+
+    this.mockItems.push(new ListEntryModel());
+    this.mockItems[0].id = "1";
+    this.mockItems[0].count = 5;
+
+    this.route.params.subscribe(params => {
+      console.log(params);
+    });
+  }
 
   ngOnInit() {
+    this.mockItems.forEach(item => {
+      let pm: any = this.getProductByID(item.id);
+      pm.count = item.count;
+      this.itemList.push(pm);
+    });
   }
 
 }
